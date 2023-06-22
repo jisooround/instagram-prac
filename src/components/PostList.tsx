c47d4f64da5b9c66c5282b28e3a2c7c073d367b0
jisooround <9utesleben@gmail.com>
2023-06-22 23:18:41 +0900

    feat: posts 기능

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,9 @@
+"use client";
 import React from "react";
+import useSWR from "swr";
 
 export default function PostList() {
+  const { data, isLoading: loading } = useSWR("/api/posts");
+  console.log("data ::: ", data);
   return <div className="w-full">PostList</div>;
 }
